23a9f518df0d95c0db835741756a70a3a480bd93
Alexander <ntwwwnt@gmail.com>
2022-12-22 02:44:59 +0300

    Fixed merge conflicts in master

--- src/app/core/VersionedXYZSource.ts
+++ src/app/core/VersionedXYZSource.ts
@@ -1,4 +1,4 @@
-import { ImageTile, Tile } from "ol";
+import { ImageTile } from "ol";
 import TileState from "ol/TileState";
 import XYZ, { type Options as XYZOptions } from "ol/source/XYZ";
 import { unref } from "vue";
@@ -8,8 +8,6 @@ import { CurrentSignalRangeResponseDTO } from "./net/dto/responseDTO";
 class VersionedXYZContactMapSource extends XYZ {
   protected sourceVersion: number;
   // protected lastResponse?: Record<string, unknown>;
-  protected tileImageSrcCache: Map<string, string>;
-  protected isCachingEnabled: boolean;
 
   constructor(
     protected readonly layersManager: HiCViewAndLayersManager,
@@ -18,56 +16,31 @@ class VersionedXYZContactMapSource extends XYZ {
   ) {
     super(xyzOptions);
     this.sourceVersion = 0;
-    this.isCachingEnabled = false;
-    this.tileImageSrcCache = new Map();
-    this.setTileLoadFunction(this.customTileLoadFunction(this));
-    this.do_reload();
-  }
-
-  /**
-   * do_reload
-   */
-  public do_reload() {
-    this.clearTileCache(true);
-    ++this.sourceVersion;
-    this.setTileUrlFunction(this.create_tile_url_function());
-    this.changed();
-  }
-
-  public clearTileCache(clearHashMap?: boolean | undefined | null) {
-    this.tileCache.expireCache({});
-    this.tileCache.clear();
-    if (clearHashMap && this.isCachingEnabled) {
-      this.tileImageSrcCache.clear();
-    }
-  }
-
-  public customTileLoadFunction(xyzSource: VersionedXYZContactMapSource) {
-    const layersManager = this.layersManager;
-    return (tile: Tile, src: string) => {
+    this.setTileLoadFunction((tile, src) => {
+      console.assert(tile instanceof ImageTile);
+      const imageTile: ImageTile = tile as ImageTile;
+      const image: HTMLImageElement | HTMLVideoElement =
+        imageTile.getImage() as HTMLImageElement | HTMLVideoElement;
       const xhr = new XMLHttpRequest();
       xhr.responseType = "json";
-      xhr.addEventListener("loadend", function () {
+      xhr.addEventListener("loadend", function (evt) {
+        // console.log("Got XHR Response: ", this.response);
         const data = this.response;
-
-        const imageTile: ImageTile = tile as ImageTile;
-        const image: HTMLImageElement =
-          imageTile.getImage() as HTMLImageElement;
-
-        if (
-          data !== null &&
-          data !== undefined &&
-          data.image !== undefined &&
-          this.status === 200
-        ) {
-          console.assert(tile instanceof ImageTile);
+        if (data && data.image) {
+          // image.src = URL.createObjectURL(data.image);
+          // image.src = "data:image/png;base64," + data.image;
+          //this.lastResponse = this.response;
+          tile["lastResponse"] = data;
           image.src = data.image;
-          if (xyzSource.isCachingEnabled) {
-            xyzSource.tileImageSrcCache.set(
-              tile.getTileCoord().toString(),
-              image.src
-            );
-          }
+          // console.log("Data.ranges is ", data.ranges);
+          // console.log(
+          //   "Constructed ranges DTO: ",
+          //   new CurrentSignalRangeResponseDTO(data.ranges)
+          // );
+          // console.log(
+          //   "Constructed ranges entity: ",
+          //   new CurrentSignalRangeResponseDTO(data.ranges).toEntity()
+          // );
           layersManager.callbackFns.contrastSliderRangesCallbacks.forEach(
             (callbackFn) => {
               callbackFn(
@@ -75,75 +48,32 @@ class VersionedXYZContactMapSource extends XYZ {
               );
             }
           );
-          tile.setState(TileState.LOADED);
+        } else if (this.status >= 400) {
+          tile.setState(TileState.ERROR);
         } else {
-          if (this.status >= 400) {
-            console.log("Error: Tile load status >= 400 for", tile);
-            // tile.setState(TileState.ERROR);
-            tile.setState(TileState.EMPTY);
-            return;
-          }
-          if (xyzSource.isCachingEnabled) {
-            const oldImageSrc = xyzSource.tileImageSrcCache.get(
-              tile.getTileCoord().toString()
-            );
-            if (oldImageSrc) {
-              image.src = oldImageSrc;
-              tile.setState(TileState.LOADED);
-              return;
-            }
-          }
-          tile.setState(TileState.EMPTY);
-          return;
+          image.src = tile["lastResponse"].image;
         }
       });
       xhr.addEventListener("error", function () {
-        console.log("onError listener xhr", this);
         if (this.status >= 400) {
-          console.log("onError listener xhr: set Error", this);
-          // tile.setState(TileState.ERROR);
+          tile.setState(TileState.ERROR);
         }
-      });
-      xhr.addEventListener("loadstart", () => {
-        tile.setState(TileState.LOADING);
-      });
-      xhr.addEventListener("abort", () => {
-        tile.setState(TileState.EMPTY);
-        console.log("xhr request was aborted: ", this);
-        const oldImageSrc = xyzSource.tileImageSrcCache.get(
-          tile.getTileCoord().toString()
-        );
-        if (oldImageSrc) {
-          ((tile as ImageTile).getImage() as HTMLImageElement).src =
-            oldImageSrc;
-        } else {
-          throw new Error(
-            "Image source is not found in cache for " +
-              tile.getTileCoord().toString()
-          );
-        }
-        tile.setState(TileState.LOADED);
-      });
-      xhr.addEventListener("timeout", () => {
-        tile.setState(TileState.EMPTY);
-        console.log("xhr request was timed out: ", this);
-        const oldImageSrc = xyzSource.tileImageSrcCache.get(
-          tile.getTileCoord().toString()
-        );
-        if (oldImageSrc) {
-          ((tile as ImageTile).getImage() as HTMLImageElement).src =
-            oldImageSrc;
-        } else {
-          throw new Error(
-            "Image source is not found in cache for " +
-              tile.getTileCoord().toString()
-          );
-        }
-        tile.setState(TileState.LOADED);
       });
       xhr.open("GET", src);
       xhr.send();
-    };
+    });
+    this.do_reload();
+  }
+
+  /**
+   * do_reload
+   */
+  public do_reload() {
+    this.tileCache.expireCache({});
+    this.tileCache.clear();
+    ++this.sourceVersion;
+    this.setTileUrlFunction(this.create_tile_url_function());
+    this.changed();
   }
 
   protected create_tile_url_function() {
