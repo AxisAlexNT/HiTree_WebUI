f26fd1aa80f17b12cee693bf9e435b68efcdc3b1
Aleksandr Serdiukov <ntwwwnt@gmail.com>
2023-08-02 22:58:21 +0300

    Added slides to the main diagonal

--- src/app/ui/components/toolbar/upper_blocks/FirstBlock.vue
+++ src/app/ui/components/toolbar/upper_blocks/FirstBlock.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="block-of-buttons">
-    <button class="btn btn-outline-primary" type="button">
-      <i class="bi bi-arrow-left-right"></i>
-    </button>
     <button
       class="btn btn-outline-primary"
       type="button"
@@ -11,7 +8,47 @@
       title="Zoom out"
       @click="zoomOut"
     >
-      <i class="bi bi-arrows-angle-expand"></i>
+      <i class="bi bi-arrows-fullscreen"></i>
+    </button>
+    <button
+      class="btn btn-outline-primary"
+      type="button"
+      data-bs-toggle="tooltip"
+      data-bs-placement="right"
+      title="Zoom In"
+      @click="zoomIn"
+    >
+      <i class="bi bi-search"></i>
+    </button>
+    <button
+      class="btn btn-outline-primary"
+      type="button"
+      data-bs-toggle="tooltip"
+      data-bs-placement="right"
+      title="Slide to the main diagonal vertically"
+      @click="slideVertically"
+    >
+      <i class="bi bi-arrow-down-up"></i>
+    </button>
+    <button
+      class="btn btn-outline-primary"
+      type="button"
+      data-bs-toggle="tooltip"
+      data-bs-placement="right"
+      title="Slide to the main diagonal horizontally"
+      @click="slideHorizontally"
+    >
+      <i class="bi bi-arrow-left-right"></i>
+    </button>
+    <button
+      class="btn btn-outline-primary"
+      type="button"
+      data-bs-toggle="tooltip"
+      data-bs-placement="right"
+      title="Slide to the main diagonal over counter-diagonal"
+      @click="slideByCounterDiagonal"
+    >
+      <i class="bi bi-arrows-angle-contract"></i>
     </button>
   </div>
 </template>
@@ -29,6 +66,58 @@ function zoomOut() {
     const view = map?.getView();
     if (map && view) {
       view.animate({ zoom: 0 });
+    }
+  }
+}
+function zoomIn() {
+  if (props.mapManager) {
+    const map = props.mapManager?.map;
+    const view = map?.getView();
+    // const center = view?.getCenter();
+    if (map && view) {
+      view.animate({ zoom: 10e10 });
+    }
+  }
+}
+
+function slideVertically() {
+  if (props.mapManager) {
+    const map = props.mapManager?.map;
+    const view = map?.getView();
+    const center = view?.getCenter();
+    if (map && view && center) {
+      view.animate({
+        center: [center[0], -center[0]],
+        zoom: view.getZoom(),
+      });
+    }
+  }
+}
+
+function slideHorizontally() {
+  if (props.mapManager) {
+    const map = props.mapManager?.map;
+    const view = map?.getView();
+    const center = view?.getCenter();
+    if (map && view && center) {
+      view.animate({
+        center: [-center[1], center[1]],
+        zoom: view.getZoom(),
+      });
+    }
+  }
+}
+
+function slideByCounterDiagonal() {
+  if (props.mapManager) {
+    const map = props.mapManager?.map;
+    const view = map?.getView();
+    const center = view?.getCenter();
+    if (map && view && center) {
+      view.animate({
+        center: [(center[0] - center[1]) / 2, -(center[0] - center[1]) / 2],
+        zoom: view.getZoom(),
+      });
     }
   }
 }
