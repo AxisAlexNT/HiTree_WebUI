c7d31bf03eee31531c020cbc10a0c48b4197e67d
Aleksandr Serdiukov <ntwwwnt@gmail.com>
2024-04-15 03:19:42 +0400

    Removed unused IGV integration code

--- src/app/ui/components/tracks/HorizontalIGVTrack.vue
+++ src/app/ui/components/tracks/HorizontalIGVTrack.vue
@@ -2,49 +2,7 @@
   <div id="horizontal-igv-track-div"></div>
 </template>
 
-<script setup lang="ts">
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-///<reference path="../../../../../node_modules/igv/dist/igv.esm.js" />
-import igv from "igv";
-import { onMounted } from "vue";
-
-const igvOptions = {
-  genome: "hg38",
-  locus: "chr8:127,736,588-127,739,371",
-  tracks: [
-    {
-      name: "HG00103",
-      url: "https://s3.amazonaws.com/1000genomes/data/HG00103/alignment/HG00103.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram",
-      indexURL:
-        "https://s3.amazonaws.com/1000genomes/data/HG00103/alignment/HG00103.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram.crai",
-      format: "cram",
-    },
-  ],
-};
-
-// watch(
-//   () => document.getElementById("horizontal-igv-track-div"),
-//   (newDiv) => {
-//     console.log("New div is", newDiv);
-//     if (newDiv) {
-//       igv.createBrowser(newDiv, igvOptions).then(function () {
-//         console.log("Created Horizontal IGV browser");
-//       });
-//     }
-//   }
-// );
-
-onMounted(() => {
-  const newDiv = document.getElementById("horizontal-igv-track-div");
-
-  if (false && newDiv) {
-    console.log("New div is", newDiv);
-    // igv.createBrowser(newDiv, igvOptions).then(function () {
-    //  console.log("Created Horizontal IGV browser");
-    // });
-  }
-});
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 #horizontal-igv-track-div {
